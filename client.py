3878138044d9266c08d4628bcaab2c7ddfbe16ce
jayrock3000 <jeffrhoten@gmail.com>
2023-11-27 11:35:19 -0800

    Client loop and input verification established

--- client.py
+++ client.py
@@ -1,16 +1,63 @@
 import socket
 
+# Variable for debugging program
+debug = True
+
+def commandInput():
+    if debug == True:
+        print("commandInput() function activated")
+
+    userInput = ""
+
+    while True:
+        print("Enter command (ls, get, put, quit): ", end="")
+        validInputs = ['ls', 'quit']
+        userInput = input()
+        try:
+            userInput = str(userInput)
+
+        except:
+            print("An error occurred with that input. Please try again.\n")
+            continue
+
+        if userInput == "":
+            print("Entry cannot be blank\n")
+            continue
+
+        elif userInput.startswith('get'):
+            return userInput
+
+        elif userInput.startswith('put'):
+            return userInput
+
+        elif userInput in validInputs:
+            return userInput
+
+        else:
+            print("Invalid command. Please try again.\n")
+            continue
+        break
+
+
 # Connect to server
 server_name = 'localhost'
-server_port = 12345
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((server_name, server_port))
+server_port = 5432
+
 
 # Send commands to the server
-command = input("Enter command (ls, get, put, quit): ")
-client_socket.send(command.encode())
+#command = input("Enter command (ls, get, put, quit): ")
+
+command = ''
+while command != 'quit':
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((server_name, server_port))
+    command = commandInput()
+    client_socket.send(command.encode())
+    client_socket.close()
+    if debug == True:
+        print("client socket has closed")
 
 # Handle server responses and file transfers
 # Your implementation for handling server responses and file transfers goes here
 
-client_socket.close()
+print("client has quit")
--- server.py
+++ server.py
@@ -2,8 +2,11 @@ import socket
 import os
 from Python.sendfile.sendfileserv import FileReceiverServer
 
+# Variable for debugging program
+debug = True
+
 # Set up socket
-server_port = 12345
+server_port = 5432
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(('localhost', server_port))
 server_socket.listen(1)
@@ -11,25 +14,38 @@ server_socket.listen(1)
 print(f"FTP Server is ready on port {server_port}")
 
 def show_listing():
+    if debug == True:
+        print("show_listing() function activated")
+    
     files = os.listdir('.')
     files = '\n'.join(files)
 
     return files
 
 def send_file():
+    if debug == True:
+        print("send_file() function activated")
     # convert or make a duplicate of FileSender in sendfilecli.py
     # to send file from server
     pass
 
 
-# Accept connections
+
+# Establish connection
 while True:
+    print("Waiting for connection...")
+    
+    # Accept connections
     connection_socket, addr = server_socket.accept()
     print(f"Connection established with {addr}")
-
+    
     # Process client commands (e.g., ls, get, put, quit)
     while True:
+        # Get command from client
         command = connection_socket.recv(1024).decode()
+        if debug == True:
+            print("command received:", end='')
+            print(command)
 
         # show files/directories in server
         if command == 'ls': show_listing()
@@ -41,9 +57,18 @@ while True:
         elif command.startswith('put'):
             server_receive = FileReceiverServer(server_port)
             server_receive.start_server()
+
+        elif command == 'quit':
+            connection_socket.close()
+            print("server socket has closed")
+            print("server has quit")
+            exit()
+
         else: 
             print("Error server-1: Unknown Command.")
             break
-        # Example: if command == "quit": break
 
-    connection_socket.close()
+        connection_socket.close()
+        if debug == True:
+            print("server socket has closed")
+        break
